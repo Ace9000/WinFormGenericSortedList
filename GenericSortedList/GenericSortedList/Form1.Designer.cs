e68902b3d3999de06f32222b25baa81d46d44266
Anastas Acevski <123334501+Ace9000@users.noreply.github.com>
2023-03-31 00:03:52 +0200

    Add files via upload

---
+++ GenericSortedList/GenericSortedList/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace GenericSortedList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button11 = new System.Windows.Forms.Button();
+            this.ValueListBox = new System.Windows.Forms.ListBox();
+            this.keyListBox = new System.Windows.Forms.ListBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txtKeyy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(224, 118);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(185, 37);
+            this.button11.TabIndex = 55;
+            this.button11.Text = "For Each By SortedList";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // ValueListBox
+            // 
+            this.ValueListBox.FormattingEnabled = true;
+            this.ValueListBox.ItemHeight = 16;
+            this.ValueListBox.Location = new System.Drawing.Point(240, 311);
+            this.ValueListBox.Name = "ValueListBox";
+            this.ValueListBox.Size = new System.Drawing.Size(187, 260);
+            this.ValueListBox.TabIndex = 54;
+            // 
+            // keyListBox
+            // 
+            this.keyListBox.FormattingEnabled = true;
+            this.keyListBox.ItemHeight = 16;
+            this.keyListBox.Location = new System.Drawing.Point(22, 311);
+            this.keyListBox.Name = "keyListBox";
+            this.keyListBox.Size = new System.Drawing.Size(187, 260);
+            this.keyListBox.TabIndex = 53;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(273, 258);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 35);
+            this.button10.TabIndex = 52;
+            this.button10.Text = "Find the Value";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue.Location = new System.Drawing.Point(94, 265);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(169, 24);
+            this.txtValue.TabIndex = 51;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Find Value:";
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(273, 212);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 35);
+            this.button9.TabIndex = 49;
+            this.button9.Text = "Find the Key";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // txtKeyy
+            // 
+            this.txtKeyy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeyy.Location = new System.Drawing.Point(95, 218);
+            this.txtKeyy.Name = "txtKeyy";
+            this.txtKeyy.Size = new System.Drawing.Size(169, 24);
+            this.txtKeyy.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Find Key:";
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(105, 178);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 34);
+            this.button8.TabIndex = 46;
+            this.button8.Text = "Copy To";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(24, 178);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 32);
+            this.button7.TabIndex = 45;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(24, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 37);
+            this.button6.TabIndex = 44;
+            this.button6.Text = "For Loop by SortedList";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(373, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 29);
+            this.button5.TabIndex = 43;
+            this.button5.Text = "Get Size";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(296, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 29);
+            this.button4.TabIndex = 42;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(219, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 29);
+            this.button3.TabIndex = 41;
+            this.button3.Text = "Write";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(142, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 29);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 18);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Key";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(49, 70);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(85, 24);
+            this.txtKey.TabIndex = 38;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 38);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "Add Items by Sortedlist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 589);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.ValueListBox);
+            this.Controls.Add(this.keyListBox);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.txtKeyy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.ListBox ValueListBox;
+        private System.Windows.Forms.ListBox keyListBox;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txtKeyy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GenericSortedList/GenericSortedList/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericSortedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            System.Collections.Generic.SortedList<Int32, string> sl = new System.Collections.Generic.SortedList<Int32, string>();
+
+            sl.Add(23, "Avion");
+            sl.Add(43, " Brod");
+            sl.Add(29, "Traktor");
+            sl.Add(54, "Tocak0");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            System.Collections.Generic.SortedList<Int32, string> sl = new System.Collections.Generic.SortedList<Int32, string>();
+
+            sl.Add(23, "Avion");
+            sl.Add(43, " Brod");
+            sl.Add(29, "Traktor");
+            sl.Add(54, "Tocak0");
+
+            Int32 Key;
+            Int32.TryParse(txtKey.Text, out Key);
+            Text = sl[Key];
+
+            Text = sl.Values[1];
+
+            Text = sl.Keys[1].ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            System.Collections.Generic.SortedList<Int32, string> sl = new System.Collections.Generic.SortedList<Int32, string>();
+
+            sl.Add(23, "Avion");
+            sl.Add(43, " Brod");
+            sl.Add(29, "Traktor");
+            sl.Add(54, "Tocak0");
+
+            Int32 Key;
+            Int32.TryParse(txtKey.Text, out Key);
+            sl[Key] = "Test";
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            System.Collections.Generic.SortedList<Int32, string> sl = new System.Collections.Generic.SortedList<Int32, string>();
+
+            sl.Add(23, "Avion");
+            sl.Add(43, " Brod");
+            sl.Add(29, "Traktor");
+            sl.Add(54, "Tocak0");
+
+            foreach (var item in sl)
+            {
+                keyListBox.Items.Add(item.Key);
+                ValueListBox.Items.Add(item.Value);
+            }
+
+            foreach (KeyValuePair<Int32, string> item in sl)
+            {
+                keyListBox.Items.Add(item.Key);
+                ValueListBox.Items.Add(item.Value);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+           
+            System.Collections.Generic.SortedList<Int32, string> sl = new System.Collections.Generic.SortedList<Int32, string>();
+
+            sl.Add(23, "Avion");
+            sl.Add(43, " Brod");
+            sl.Add(29, "Traktor");
+            sl.Add(54, "Tocak0");
+
+            KeyValuePair <Int32, string> [] target_arrey = new KeyValuePair <int, string> [sl.Count()];
+            target_arrey = sl.ToArray();
+
+            Int32[] target_keys = new Int32[sl.Keys.Count()];
+            target_keys = sl.Keys.ToArray();
+
+            string[] target_values = new string [sl.Values.Count()];
+            target_values = sl.Values.ToArray();
+        }
+
+    }
+}
